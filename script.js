4e1e30a27de186830514fc937455b80b8d865cee
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-01 21:27:08 +0300

    Create files and boilerplate. Sart with lesson and continue with assignment.

---
+++ script.js
@@ -0,0 +1,59 @@
+// Example of what is clean code with same functionality
+
+const x = function(z) {
+    const w = "Hello";
+    return w + z
+}
+
+x("John");
+
+// or
+
+const generateUsergreetings = function(name){
+    const greeting = "Hello";
+    return greeting + name;
+};
+
+generateUsergreetings("John");
+
+
+// Which one is better and more readable?!
+
+
+
+// Tips to implement:
+
+// - single characters can be used as variable names in the context of a loop or a callback function,
+// but its a good practice to avoid them anywhere else
+
+
+// - usage of camelCase is very common in JS (but it may sometimes vary from company to company)
+
+// - a good name for functions and variables are descriptive, meaning that they self-describe what 
+// they (e.g generateUserGreetings(), createRandomNumber(), etc).
+
+// - consistency allows for predictability, e.g example below: 
+
+function getPlayerScore(){};
+function getPlayerName(){};
+function getPlayerTag(){};
+
+// or 
+
+function getUserScore(){};
+function fetchPlayerName(){};
+function retrievePlayer1Tag(){};
+
+
+// in first example function's behavior is easier to predict as its name creates sort of 
+// pattern/grouping (in example 1 its clearly seen that funstions collect data about player).
+
+
+// - variables represent "things", so their name should be/begin a noun or adjective, meanwhile
+// functions represent action, so they should be named with a verb
+
+const defaultColor = "Red";
+let numberOfArrays = 10;
+
+function createFolder() {};
+function findSuitableName(){};
