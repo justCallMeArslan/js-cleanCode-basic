1acb44cca9d390eb5e01a83e3922212ec3a4dae9
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-02 03:51:50 +0300

    Complete lesson and assignment.

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function createFolder() {};
 function findSuitableName(){};
 
 
-// - use searchable and immediately understanble names:
+// - use searchable and immediately understandable names:
 
 setTimeout(stopTimer, 3600000); // whats that magical 3600000?! 
 
@@ -84,5 +84,62 @@ setTimeout(stopTimer, ONE_HOUR);
 // because it creates a bloat which doesnt belong there.
 
 
-//Great code comes from experience. Experience comes from not-so-great code.
 
+// Assignments part
+
+
+
+
+// 11 principles to keep code clean:
+
+
+// 1. Revise logic before coding -  flow diagrams and written pseudocode is great way to avoid mess.
+// 2. Clearly expose the structure of the page. Using IDs for main containers can be helpful.
+// 3. Use the correct indentations with consistency over the page and code. 
+// 4. Write explanatory comments - its good.
+// 5. Avoid abusing comments. examples of good comments: 
+
+// - Authoring specifications (e.g. /* Coded by John, November 13th 2010 */).
+// - Detailed statements on the functionality of a method or procedure (e.g. /* This function validates 
+// the login form with the aid of the e-mail check function */).
+// - Quick notifications or labels that state where a recent change was made (e.g. /* Added e-mail 
+// validation procedure */).
+
+// 6. Avoid extremely large functions. Advice here is to break up large functions into smaller one. 
+// 7. Use naming standards for naming for functions and variables. 
+// 8. Treat changes with caution. 
+// 9. Avoid indiscriminate Mixing of coding languages. Avoid mixing "in-styles" and try to separate 
+// and segregate HTML/CSS/JS.
+// 10. Summarize your imports. Dont overuse importing/creating many style sheets or js script files.
+// It may be better to combine 4-5 files into 1-2 files and import together.  
+// 11. Dont forget about efficiency. 
+
+
+
+
+// Coding horror
+
+
+// 1. Code can only tell you how the program works; comments can tell you why it works. 
+
+// 2. the best kind of comments are the ones you don’t need. Allow me to. you should first strive to 
+// make your code as simple as possible to understand without relying on comments as a crutch. 
+// at the point where the code cannot be made easier to understand should you begin to add comments.
+
+// 3. You should always write your code as if comments didn’t exist - that way youcode will be written
+// in the simplest, plainest and most self-documenting way.
+
+
+// 4. if your feel your code is too complex to understand without comments, your code is probably 
+// just bad. Rewrite it until it doesn’t need comments any more.
+
+
+
+
+// Martin Fowler
+
+
+// Treat code as its a documentation. So it should be self-describing and well self-explainatory.
+
+// You can program in your own style on your personal projects, but anything you do in a team 
+// should follow the needs of that team.
